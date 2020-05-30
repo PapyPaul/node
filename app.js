331549c6dbcf3885181133a88088ef8396361a90
PapyPaul <kpbukasa@yahoo.com>
2020-05-30 07:59:22 +0200

    POST request

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+var data ="No LTE Data Availabe";
 
 // use the express-static middleware
 app.use(express.static("public"));
+app.use(bodyParser.json());
+
 app.get('/',(req,res)=>{
     res.send("Welcome to the home page ");
+});
+
+app.post('/',(req,res)=>{
+    data = req.body;
+     res.send(data);
 })
 
 const port = process.env.PORT||80;
