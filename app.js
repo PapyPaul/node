0faa5ce405944c289996febe338c9bf8938e540d
PapyPaul <kpbukasa@yahoo.com>
2020-05-30 05:54:43 +0200

    Added use express

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 
 
+// use the express-static middleware
+app.use(express.static("public"));
 app.get('/',(req,res)=>{
     res.send("Welcome to the home page ");
 })
 
-const port = process.env.port||80;
+const port = process.env.PORT||80;
 
 app.listen(port,()=>{
     console.log('Papy Your Server is Listening from Port 3000');
